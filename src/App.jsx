da44aa70280eeeab024f7b3be7b0d3cccf800b10
richardbrownmiami-commits <richard.brown.miami@gmail.com>
2026-06-10 00:44:22 +0530

    update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout/Layout'
 import ChatPage from './pages/ChatPage'
 import BrainPage from './pages/BrainPage'
 import MonitorPage from './pages/MonitorPage'
+import PromptPage from './pages/PromptPage'
 import SettingsPage from './pages/SettingsPage'
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/" element={<ChatPage />} />
           <Route path="/brain" element={<BrainPage />} />
           <Route path="/monitor" element={<MonitorPage />} />
+          <Route path="/prompt" element={<PromptPage />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>

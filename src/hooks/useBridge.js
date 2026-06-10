acef9925b1d9284001612c0f62153eadb93b7cf0
richardbrownmiami-commits <richard.brown.miami@gmail.com>
2026-06-10 14:42:03 +0530

    Add startSTT to native bridge hook

--- src/hooks/useBridge.js
+++ src/hooks/useBridge.js
@@ -2,9 +2,10 @@ const bridge = window.AndroidBridge || {};
 
 export const useBridge = () => ({
   requestPermission: bridge.reqPerm || (() => Promise.resolve(false)),
-  takePhoto: bridge.takePic || (() => Promise.resolve('')),
-  getLocation: bridge.getLoc || (() => Promise.resolve('')),
-  getDeviceInfo: bridge.getDev || (() => Promise.resolve('{}')),
+  takePhoto: bridge.takePic || (() => Promise.resolve("")),
+  getLocation: bridge.getLoc || (() => Promise.resolve("")),
+  getDeviceInfo: bridge.getDev || (() => Promise.resolve("{}")),
+  startSTT: bridge.stt || (() => Promise.resolve("")),
   speakText: bridge.speak || (() => {}),
   notify: bridge.notify || (() => {}),
   isAvailable: !!window.AndroidBridge,
